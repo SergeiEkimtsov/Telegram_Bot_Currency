692c692b53b0f7138a165cba6f10919acabb1734
Sergei Ekimtsov <Sergei EkimtsovSS@gmail.com>
2024-05-22 19:37:56 +0300

    Correcting parsing method

--- src/main/java/com/ekimtsov/telegram_bot_20/sevices/ParsingWebPageJsoup.java
+++ src/main/java/com/ekimtsov/telegram_bot_20/sevices/ParsingWebPageJsoup.java
@@ -13,55 +13,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ParsingWebPageJsoup implements CurrencyModeServicies {
-    public static String parsingData(String data) throws Exception {
 
-        Pattern pattern = Pattern.compile("\\d{2}\\.\\d{2}");
-
-        Matcher matcher = pattern.matcher(data);
-        if (matcher.find()) return matcher.group() + ".2024";
-        else throw new Exception("No matching of data");
-    }
-
-    // public static void main(String[] args) throws MalformedURLException {
-    public static String respondWeather() throws Exception {
-        List<String> list = new ArrayList<>();
-        list.add("Время дня:        ");
-        list.add("Погодные явления: ");
-        list.add("Температура:      ");
-        list.add("Давление:         ");
-        list.add("Отн.Влажность:    ");
-        list.add("Ветер:            ");
-        String respond = "";
-        String url = "https://pogoda.spb.ru/";
-        Document page;
-        try {
-            page = Jsoup.connect(url).get();
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        Element table = page.select("table[class=wt]").first();
-        //System.out.println(table);
-        Element name = table.select("tr[class=wth]").first();
-        System.out.println(name);
-        Elements values = table.select("tr[valign=top]");
-        //System.out.println(values.size());
-        String data = name.select("th[id=dt]").first().text();
-//          System.out.println(data);
-
-        respond += "Дата: " + parsingData(data) + "\n";
-        //System.out.println(dataOut);
-
-        Element line = values.get(0);
-        //System.out.println(line);
-        int i=0;
-
-        for (Element s : line.select("td")) {
-            respond += list.get(i)+s.text()+"\n";
-            i++;
-        }
-        return respond;
-    }
     public  static  String getAllCurrencyRates(){
         String url = "https://cbr.ru/currency_base/daily/";
         Document document;
